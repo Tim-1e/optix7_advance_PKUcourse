17aeb47dbd17322381dfc5eb180042a5d46801ba
Bingxuan_Li@PKU <lbx@stu.pku.edu.cn>
2022-11-15 19:09:08 +0800

    experiment

--- raytrace_work/config.h
+++ raytrace_work/config.h
@@ -1,10 +1,10 @@
 # pragma once
 # define VISUALIZE true
 # define DOWNLOAD 1
-# define DOWNLOAD_RAW 0
+# define DOWNLOAD_RAW 1
 # define DOWNLOAD_DIR "../../output/"
 
-# define GT_ENABLE 1
+# define GT_ENABLE 0
 # define GT_DIR "../../ground_truth/300s.rawFrame"
 
 # define FIXED_CAMERA 1
@@ -17,4 +17,4 @@
 # define MY_BRDF 0
 # define MY_NEE 1
 # define MY_MIS 2
-# define MY_MODE MY_NEE
+# define MY_MODE MY_MIS
